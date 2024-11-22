98d1dd29906ab969511c79943f0d2843b2a10992
Andry510 <andrymonteleon@gmail.com>
2024-11-22 01:54:22 -0600

    Creación de messages
    Se creo una clase en el cual almacena mensajes para reutilizar

---
+++ Messages/Messages.cs
@@ -0,0 +1,44 @@
+﻿namespace store.Messages
+{
+    public static class MessagesClass
+    {
+         // Mensajes de éxito
+        public static string SuccessCreate() =>
+           "El registro ha sido creado exitosamente.";
+
+        public static string SuccessUpdate() =>
+            "El registro ha sido actualizado exitosamente.";
+
+        public static string SuccessDelete() =>
+            "El registro ha sido eliminado exitosamente.";
+
+        public static string SuccessGet() =>
+            "Los datos han sido obtenidos exitosamente.";
+
+        public static string SuccessNoContent() =>
+            "No se encontraron datos para mostrar.";
+
+        // Mensajes de error
+        public static string ErrorCreate() =>
+            "Hubo un error al intentar crear el registro.";
+
+        public static string ErrorUpdate() =>
+            "Hubo un error al intentar actualizar el registro.";
+
+        public static string ErrorDelete() =>
+            "Hubo un error al intentar eliminar el registro.";
+
+        public static string ErrorGet() =>
+            "Hubo un error al intentar obtener los datos.";
+
+        public static string ErrorNoContent() =>
+            "No se pudo encontrar contenido para la solicitud.";
+
+        // Mensajes relacionados con la existencia de elementos
+        public static string NotFound() =>
+            "El elemento solicitado no fue encontrado.";
+
+        public static string AlreadyExists() =>
+            "El elemento ya existe.";
+    }
+}
