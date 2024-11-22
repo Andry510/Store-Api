0f808dd7f50c9ab7a11b858831d2e82dccc05078
Andry510 <andrymonteleon@gmail.com>
2024-11-22 01:48:55 -0600

    Configuración en mi context
    Configure mi DataBaseContext para que tenga el FOreignKey del perfil y que se elimine automaticamente

--- Contexts/DataBaseContext.cs
+++ Contexts/DataBaseContext.cs
@@ -10,4 +10,13 @@ public class DataBaseContext : DbContext
     public DbSet<Authentication> Authentications { get; set; } = null!;
     public DbSet<Profile> Profiles { get; set; } = null!;
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Authentication>()
+        .HasOne(a => a.Profile)
+        .WithOne()
+        .HasForeignKey<Authentication>(a => a.ProfileId)
+        .OnDelete(DeleteBehavior.Cascade);
+    }
+
 }
