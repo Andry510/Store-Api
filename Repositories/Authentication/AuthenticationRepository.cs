93622c1e99f4373cac3c451e0f698753b69da809
Andry510 <andrymonteleon@gmail.com>
2024-11-22 01:57:49 -0600

    Creación del Repositorio de autentificación
    Se creo una carpeta para la autentificación y poder relaciónarse con la base de datos

---
+++ Repositories/Authentication/AuthenticationRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using store.Contexts;
+using store.Interfaces;
+using store.Models;
+
+namespace store.Repositories;
+
+public class AuthenticationRepository : IAuthenticationRepository
+{
+    private readonly DataBaseContext db;
+
+    public AuthenticationRepository(DataBaseContext DBContext)
+    {
+        db = DBContext;
+    }
+    public Task<Authentication> Save()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Authentication> FindOneById()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Authentication?> FindOneByEmail(string email)
+    {
+       var data = await db.Authentications.FirstOrDefaultAsync(a => a.Email == email);
+       return data;
+    }
+}
