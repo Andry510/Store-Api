6c2901bb419e9873c9eb7bd3c691a83971ee27ea
Andry510 <andrymonteleon@gmail.com>
2024-11-22 01:50:49 -0600

    Modificación del Dto de Autentificación
    Se implemento mensajes de respuesta

--- Dtos/Authentication/CreateDto.cs
+++ Dtos/Authentication/CreateDto.cs
@@ -1,24 +1,29 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using store.Enum;
 
-namespace store.Dtos.Authentication;
-
-public class CreateProfileDto
+namespace store.Dtos.Authentication
 {
-    [Required]
-    [RegularExpression(@"^[a-zA-Z\s'-]+$")]
-    public required string Name { get; set; }
+    public class CreateProfileDto
+    {
+        [Required(ErrorMessage = "Nombre obligatorio.")]
+        [StringLength(50, ErrorMessage = "Máximo 50 caracteres.")]
+        [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s'-]+$", ErrorMessage = "Solo letras y caracteres especiales.")]
+        public required string Name { get; set; }
 
-    [Required]
-    [RegularExpression(@"^[a-zA-Z\s'-]+$")]
-    public required string LastName { get; set; }
+        [Required(ErrorMessage = "Apellido obligatorio.")]
+        [StringLength(50, ErrorMessage = "Máximo 50 caracteres.")]
+        [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s'-]+$", ErrorMessage = "Solo letras y caracteres especiales.")]
+        public required string LastName { get; set; }
 
-    [Required]
-    [EmailAddress]
-    public required string Email { get; set; }
+        [Required(ErrorMessage = "Correo obligatorio.")]
+        [EmailAddress(ErrorMessage = "Correo inválido.")]
+        public required string Email { get; set; }
 
-    [MinLength(6)]
-    public required string Password { get; set; }
+        [Required(ErrorMessage = "Contraseña obligatoria.")]
+        [MinLength(6, ErrorMessage = "Mínimo 6 caracteres.")]
+        [StringLength(50, ErrorMessage = "Máximo 50 caracteres.")]
+        public required string Password { get; set; }
 
-    public Role? Rol { get; set; } = Role.Employee;
+        public Role? Rol { get; set; } = Role.Employee;
+    }
 }
