3411d93635f368ac1354d979f7f7b43a06733ce6
Andry510 <andrymonteleon@gmail.com>
2024-11-23 01:20:32 -0600

    Modificación del Dto de crear usuario
    Se modifico la manera en que sé usaba los metadatos

--- Dtos/Authentication/CreateDto.cs
+++ Dtos/Authentication/CreateDto.cs
@@ -1,29 +1,24 @@
-﻿using System.ComponentModel.DataAnnotations;
-using store.Enum;
+﻿using System.Text.Json.Serialization;
+using store.Enums;
 
-namespace store.Dtos.Authentication
+namespace store.Dtos.Authentication;
+
+public class CreateProfileDto
 {
-    public class CreateProfileDto
-    {
-        [Required(ErrorMessage = "Nombre obligatorio.")]
-        [StringLength(50, ErrorMessage = "Máximo 50 caracteres.")]
-        [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s'-]+$", ErrorMessage = "Solo letras y caracteres especiales.")]
-        public required string Name { get; set; }
+    [ValidatorBody([BodyOptions.Required, BodyOptions.IsString])]
+    public string Name { get; set; } = null!;
 
-        [Required(ErrorMessage = "Apellido obligatorio.")]
-        [StringLength(50, ErrorMessage = "Máximo 50 caracteres.")]
-        [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s'-]+$", ErrorMessage = "Solo letras y caracteres especiales.")]
-        public required string LastName { get; set; }
+    [ValidatorBody([BodyOptions.Required, BodyOptions.IsString])]
+    public string LastName { get; set; } = null!;
 
-        [Required(ErrorMessage = "Correo obligatorio.")]
-        [EmailAddress(ErrorMessage = "Correo inválido.")]
-        public required string Email { get; set; }
+    [ValidatorBody([BodyOptions.Required, BodyOptions.IsEmail])]
+    public string Email { get; set; } = null!;
 
-        [Required(ErrorMessage = "Contraseña obligatoria.")]
-        [MinLength(6, ErrorMessage = "Mínimo 6 caracteres.")]
-        [StringLength(50, ErrorMessage = "Máximo 50 caracteres.")]
-        public required string Password { get; set; }
+    [ValidatorBody([BodyOptions.Required, BodyOptions.IsPassword])]
+    public string Password { get; set; } = null!;
 
-        public Role? Rol { get; set; } = Role.Employee;
-    }
+    [ValidatorBody([BodyOptions.IsRole])]
+    [JsonConverter(typeof(JsonStringEnumConverter<Role>))]
+    public Role? Rol { get; set; } = Role.Customer;
 }
+
